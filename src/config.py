59432436d00b1dacfd85c8aca0213f6daafb12d1
Irina <92697078+10100111@users.noreply.github.com>
2022-09-14 19:42:43 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 TOKEN = '5400431421:AAGR-1aN3bVQdwjNIdgfNv_J-YCRJZ0Hi58'
 model_path = 'src/model/model_state.pth'
-endpoint = 'https://styletransfer-bot.herokuapp.com/'
+endpoint = 'https://aiogramechobotwebhookyanki.herokuapp.com/'
 herokuapi = '775acb09-665d-45a5-a77c-a976a8b4f877'
 
